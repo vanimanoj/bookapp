491e3732ca7de50ba0e8ea6d33d386649becdca7
vaniammu@123 <vanimanoj1997@gmail.com>
2022-05-17 00:30:05 +0530

    searchbook

--- src/components/Searchbook.js
+++ src/components/Searchbook.js
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Searchbook = () => {
   return (
-    <div>Searchbook</div>
+    <div><div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" className="form-label">Books Title</label>
+                    <input type="text" className="form-control"/>
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <button className="btn btn-success">SEARCH</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div></div>
   )
 }
 
