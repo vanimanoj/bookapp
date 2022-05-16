dd64484db4c4b09190753dc269b0465c17587f26
vaniammu@123 <vanimanoj1997@gmail.com>
2022-05-17 00:17:54 +0530

    addbook

---
+++ src/components/Addbooks.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Addbooks = () => {
+  return (
+    <div><div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+                <h1>AddBook</h1>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" className="form-label">Booktitle</label>
+                    <input type="text" className="form-control"/>
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label for="" className="form-label">Author</label>
+                    <input type="text" className="form-control"/>
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label for="" className="form-label">Price</label>
+                    <input type="text" className="form-control"/>
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <button className="btn btn-success">SUBMIT</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div></div>
+  )
+}
+
+export default Addbooks
